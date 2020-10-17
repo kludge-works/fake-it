6e4d5ef1d5a291a0579b2d390f4614bfbd82f98a
Tim Sparg <timothy.sparg@gmail.com>
2020-10-17 15:48:04 +0200

    initial fake it

---
+++ skill.ts
@@ -0,0 +1,36 @@
+import {
+	Category,
+	ParameterType,
+	ParameterVisibility,
+	resourceProvider,
+	skill,
+} from "@atomist/skill";
+
+export const Skill = skill<{ schedule: any }>({
+	name: "fake-it-skill",
+	namespace: "kludge-works",
+	displayName: "Fake It",
+	author: "Atomist",
+	categories: [Category.DevOps],
+	license: "Apache-2.0",
+
+	runtime: {
+		memory: 512,
+		timeout: 540,
+	},
+
+	resourceProviders: {
+		chat: resourceProvider.chat({ minRequired: 1 }),
+	},
+
+	parameters: {
+		schedule: {
+			type: ParameterType.Schedule,
+			displayName: "Cron check interval",
+			defaultValue: "15 * * * *",
+			description: "Cron expression to check docker registries",
+			required: false,
+			visibility: ParameterVisibility.Normal,
+		},
+	},
+});
