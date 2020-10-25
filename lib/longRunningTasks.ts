ff8bec81ad926247c75d051e6b2ba5a0464d4958
Tim Sparg <timothy.sparg@gmail.com>
2020-10-25 18:46:16 +0100

    Multiple steps

--- lib/longRunningTasks.ts
+++ lib/longRunningTasks.ts
@@ -1,12 +1,17 @@
 import { CommandContext, Step } from "@atomist/skill";
 
-export const sleep: Step<CommandContext> = {
-	name: "sleep for 20 seconds",
-	run: async (ctx, params) => {
-		await new Promise(r => setTimeout(r, 20000));
-		return {
-			code: 0,
-			reason: "Success",
-		};
-	},
-};
+export function sleep(
+	stepName: string,
+	sleepTime: number,
+): Step<CommandContext> {
+	return {
+		name: stepName,
+		run: async (ctx, params) => {
+			await new Promise(r => setTimeout(r, sleepTime));
+			return {
+				code: 0,
+				reason: "Success",
+			};
+		},
+	};
+}
