a393bda839a7ec571b311f0bdd66d8e3684890ea
Tim Sparg <timothy.sparg@gmail.com>
2020-10-25 20:06:09 +0100

    add original style long task

--- lib/longRunningTasks.ts
+++ lib/longRunningTasks.ts
@@ -17,3 +17,14 @@ export function sleep(
 		},
 	};
 }
+
+export const sleep2: Step<CommandContext> = {
+	name: "sleep for 20 seconds",
+	run: async (ctx, params) => {
+		await new Promise(r => setTimeout(r, 20000));
+		return {
+			code: 0,
+			reason: "Success",
+		};
+	},
+};
