25ad49db0117d0358831769ed45eeff747cd1a9d
Tim Sparg <timothy.sparg@gmail.com>
2020-11-01 21:45:02 +0100

    only run 1 task

--- lib/longRunningTasks.ts
+++ lib/longRunningTasks.ts
@@ -27,17 +27,6 @@ export function sleep(
 	};
 }
 
-export const sleep2: Step<CommandContext> = {
-	name: "sleep for 20 seconds",
-	run: async (ctx, params) => {
-		await new Promise(r => setTimeout(r, 20000));
-		return {
-			code: 0,
-			reason: "Success",
-		};
-	},
-};
-
 export async function slackUpdate<
 	C extends EventContext<OnPushSubscription, fakeConfiguration>
 >(
