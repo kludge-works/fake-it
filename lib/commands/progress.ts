41f0aea2923f61c2d2ffeb8a7cb524be5ebc32b1
Tim Sparg <timothy.sparg@gmail.com>
2020-11-07 10:55:49 +0100

    add more progress tasks

--- lib/commands/progress.ts
+++ lib/commands/progress.ts
@@ -1,20 +1,70 @@
 import { CommandHandler, slack } from "@atomist/skill";
 import * as _ from "lodash";
+import { sleep } from "../longRunningTasks";
 
 export const handler: CommandHandler = async ctx => {
 	const channel = _.get(ctx.trigger.source, "slack.channel.name");
 	const msgId = _.get(ctx.trigger.source, "slack.message.id");
-	const msg = slack.progressMessage(
-		"A progess message",
-		"Some progress",
-		{
-			state: "requested",
-			total: 1,
-			count: 1,
-		},
-		ctx,
+
+	await ctx.message.send(
+		slack.progressMessage(
+			"A progress message",
+			"Some progress",
+			{
+				state: "requested",
+				total: 1,
+				count: 1,
+			},
+			ctx,
+		),
+		{ channels: channel, users: [] },
+		{ msgId },
 	);
-	await ctx.message.send(msg, { channels: channel, users: [] }, { msgId });
+	await sleep(10);
+	await ctx.message.send(
+		slack.progressMessage(
+			"A progress message",
+			"approved",
+			{
+				state: "approved",
+				total: 1,
+				count: 1,
+			},
+			ctx,
+		),
+		{ channels: channel, users: [] },
+		{ msgId },
+	);
+	await sleep(10);
+	await ctx.message.send(
+		slack.progressMessage(
+			"A progress message",
+			"n process",
+			{
+				state: "in_process",
+				total: 1,
+				count: 1,
+			},
+			ctx,
+		),
+		{ channels: channel, users: [] },
+		{ msgId },
+	);
+	await sleep(10);
+	await ctx.message.send(
+		slack.progressMessage(
+			"A progress message",
+			"n process",
+			{
+				state: "success",
+				total: 1,
+				count: 1,
+			},
+			ctx,
+		),
+		{ channels: channel, users: [] },
+		{ msgId },
+	);
 
 	return {
 		code: 0,
--- lib/longRunningTasks.ts
+++ lib/longRunningTasks.ts
@@ -27,7 +27,7 @@ function msleep(n) {
 	Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, n);
 }
 
-function sleep(n) {
+export function sleep(n) {
 	msleep(n * 1000);
 }
 
