4f42412daf62b8ce28bfa2faa759c7e125a38aa3
Tim Sparg <timothy.sparg@gmail.com>
2020-11-20 22:36:41 +0100

    try a modal

--- lib/commands/provision.ts
+++ lib/commands/provision.ts
@@ -1,33 +1,48 @@
 import { CommandHandler } from "@atomist/skill";
-import { ParameterObjectValue } from "@atomist/skill/lib/prompt";
+import { ts } from "@atomist/skill/lib/slack";
+import { slack } from "@atomist/skill";
+import {
+	ActionsBlock,
+	InputBlock,
+	PlainTextElement,
+	SlackModal,
+} from "@atomist/slack-messages";
 
-interface ProvisionAction {
-	users: string;
-	environmentName: string;
-	action: "ignore" | "delete";
-	numberMachines: number;
-}
+// interface ProvisionAction {
+// 	users: string;
+// 	environmentName: string;
+// 	action: "ignore" | "delete";
+// 	numberMachines: number;
+// }
 
 export const handler: CommandHandler = async ctx => {
 	// const channel = _.get(ctx.trigger.source, "slack.channel.name");
 	await ctx.audit.log(JSON.stringify(ctx));
 
-	// const msgId = ts();
-	const response = await ctx.parameters.prompt<ProvisionAction>(
-		{
-			environmentName: {
-				displayName: "environment Name",
-			} as ParameterObjectValue,
-			users: { defaultValue: "boris" },
-			action: {},
-			numberMachines: {
-				description: "Number of Machines",
-				type: "number",
-			},
-		},
-		{},
-	);
+	const msgId = ts();
+	const msg = buildMessage();
+	const response = await ctx.message.respond(msg, {
+		id: msgId.toString(),
+		ts: msgId,
+	});
+
 	await ctx.audit.log(JSON.stringify(response));
+
+	// const response = await ctx.parameters.prompt<ProvisionAction>(
+	// 	{
+	// 		environmentName: {
+	// 			displayName: "environment Name",
+	// 		} as ParameterObjectValue,
+	// 		users: { defaultValue: "boris" },
+	// 		action: {},
+	// 		numberMachines: {
+	// 			description: "Number of Machines",
+	// 			type: "number",
+	// 		},
+	// 	},
+	// 	{},
+	// );
+	// await ctx.audit.log(JSON.stringify(response));
 	// await ctx.message
 	// 	.send(
 	// 		buildMessage(),
@@ -45,6 +60,95 @@ export const handler: CommandHandler = async ctx => {
 	// 	reason: "Success",
 	// };
 };
+
+function buildMessage(): slack.SlackMessage {
+	return {
+		title: {
+			type: "plain_text",
+			text: "Modal Title",
+		},
+		submit: {
+			type: "plain_text",
+			text: "Submit",
+		},
+		blocks: [
+			{
+				type: "input",
+				element: {
+					type: "plain_text_input",
+					action_id: "title",
+					placeholder: {
+						type: "plain_text",
+						text: "What do you want to ask of the world?",
+					},
+				},
+				label: {
+					type: "plain_text",
+					text: "Title",
+				},
+			} as InputBlock,
+			{
+				type: "input",
+				element: {
+					type: "multi_channels_select",
+					action_id: "channels",
+					placeholder: {
+						type: "plain_text",
+						text: "Where should the poll be sent?",
+					},
+				},
+				label: {
+					type: "plain_text",
+					text: "Channel(s)",
+				},
+			},
+			{
+				type: "input",
+				element: {
+					type: "plain_text_input",
+					action_id: "option_1",
+					placeholder: {
+						type: "plain_text",
+						text: "First option",
+					},
+				} as PlainTextElement,
+				label: {
+					type: "plain_text",
+					text: "Option 1",
+				},
+			} as InputBlock,
+			{
+				type: "input",
+				element: {
+					type: "plain_text_input",
+					action_id: "option_2",
+					placeholder: {
+						type: "plain_text",
+						text: "How many options do they need, really?",
+					},
+				},
+				label: {
+					type: "plain_text",
+					text: "Option 2",
+				},
+			} as InputBlock,
+			{
+				type: "actions",
+				elements: [
+					{
+						type: "button",
+						action_id: "add_option",
+						text: {
+							type: "plain_text",
+							text: "Add another option  ",
+						},
+					},
+				],
+			} as ActionsBlock,
+		],
+		type: "modal",
+	} as SlackModal;
+}
 
 // function buildMessage(): slack.SlackMessage {
 // 	return {
