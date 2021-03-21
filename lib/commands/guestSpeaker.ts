e50726569efd169d8ec46a8a8b603e703b9f6927
Tim Sparg <timothy.sparg@gmail.com>
2021-03-21 22:53:46 +0100

    Revert "fix errors"
    This reverts commit 4c72768f

--- lib/commands/guestSpeaker.ts
+++ lib/commands/guestSpeaker.ts
@@ -2,7 +2,7 @@ import { CommandHandler, slack } from "@atomist/skill";
 
 export const handler: CommandHandler = async ctx => {
 	await ctx.message.respond(
-		slack.successMessage("Guest Speaker", "And his friend Mike ", ctx),
+		slack.successMessage("Guest Speaker", "And his friend Mike", ctx),
 	);
 
 	return {
