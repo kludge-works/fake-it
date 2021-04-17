33091fb8dc63da73debdd4493e65b4e5ada04e4f
Tim Sparg <timothy.sparg@gmail.com>
2021-04-17 20:10:14 +0200

    A question

--- lib/commands/ipAddresser.ts
+++ lib/commands/ipAddresser.ts
@@ -1,8 +1,10 @@
 import { CommandHandler, slack } from "@atomist/skill";
 import * as _ from "lodash";
+import { bold, emoji, user } from "@atomist/slack-messages";
 
 export const handler: CommandHandler = async ctx => {
 	const raw_message = _.get(ctx.message, "request.raw_message");
+	const userId = _.get(ctx.message, "source.user.id");
 
 	await ctx.audit.log(`ctx.message: ${JSON.stringify(ctx.message)}`);
 	await ctx.audit.log(`ctx.parameters: ${JSON.stringify(ctx.parameters)}`);
@@ -15,13 +17,19 @@ export const handler: CommandHandler = async ctx => {
 
 	if ("127.0.0.1" === ipAddress) {
 		await ctx.message.respond(
-			slack.errorMessage("Theres no place like", "*no*", ctx),
+			slack.errorMessage("Theres no place like", bold("no"), ctx),
+		);
+	} else {
+		await ctx.message.respond(
+			slack.questionMessage(
+				"hmm I don't know you",
+				`${user("U018XUBKWAF")} can I give ${user(
+					"userId",
+				)} firewall access?`,
+				ctx,
+			),
 		);
 	}
-
-	await ctx.message.respond(
-		slack.questionMessage("hmm that seems bothersome", "some text", ctx),
-	);
 
 	return {
 		code: 0,
