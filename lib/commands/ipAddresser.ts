4019a950dafc8061ed81d5431bdff90c174e3db1
Tim Sparg <timothy.sparg@gmail.com>
2021-04-17 18:30:36 +0200

    add more logging

--- lib/commands/ipAddresser.ts
+++ lib/commands/ipAddresser.ts
@@ -2,9 +2,9 @@ import { CommandHandler, slack } from "@atomist/skill";
 import _ = require("lodash");
 
 export const handler: CommandHandler = async ctx => {
-	const parameters = _.get(ctx.message, "request.parameters");
-	await ctx.audit.log(JSON.stringify(parameters));
-
+	await ctx.audit.log(`ctx.message: ${JSON.stringify(ctx.message)}`);
+	await ctx.audit.log(`ctx.parameters: ${JSON.stringify(ctx.parameters)}`);
+	await ctx.audit.log(`ctx.trigger: ${JSON.stringify(ctx.trigger)}`);
 	await ctx.message.respond(
 		slack.questionMessage("hmm that seems bothersome", "some text", ctx),
 	);
