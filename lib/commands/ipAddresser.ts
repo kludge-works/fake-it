5e05d9b949af6831f3866e17cc156a388daed94d
Tim Sparg <timothy.sparg@gmail.com>
2021-04-18 17:10:15 +0200

    Give params a type

--- lib/commands/ipAddresser.ts
+++ lib/commands/ipAddresser.ts
@@ -20,33 +20,34 @@ export const handler: CommandHandler = async ctx => {
 	const userId = _.get(ctx.message, "source.slack.user.id");
 	const parentMsg = _.get(ctx.trigger.source, "slack.message.id");
 	const channel = _.get(ctx.trigger.source, "slack.channel.name");
-	const response = _.get(ctx.message, "request.parameters");
+	const response = _.get(ctx.message, "request.parameters") as Array<{
+		name: string;
+		value: string;
+	}>;
 
 	let reason: string;
-
-	const msgId = ts();
-	const msgOptions = {
-		id: msgId.toString(),
-		ts: msgId,
-		thread: parentMsg,
-	} as MessageOptions;
 
 	await ctx.audit.log(`raw_message: ${raw_message}`);
 	await ctx.audit.log(`userId: ${userId}`);
 	await ctx.audit.log(`parentMsg: ${parentMsg}`);
 	await ctx.audit.log(`channel: ${channel}`);
-	await ctx.audit.log(`msgOptions: ${stringify(msgOptions)}`);
 	await ctx.audit.log(`ctx.message: ${stringify(ctx.message)}`);
 	await ctx.audit.log(`ctx.parameters: ${stringify(ctx.parameters)}`);
 	await ctx.audit.log(`ctx.trigger: ${stringify(ctx.trigger)}`);
 	await ctx.audit.log(`request.parameters: ${stringify(response)}`);
 
 	if (response.length) {
-		const confirmation = response["confirmation"].value;
-		const ipAddress = response["ipAddress"].value;
+		const confirmation = response.find(
+			param => param.name === "confirmation",
+		).value;
+		const ipAddress = response.find(param => param.name === "ipAddress")
+			.value;
+		const messageId = response.find(param => param.name === "messageId")
+			.value;
 
 		await ctx.audit.log(`confirmation: ${stringify(confirmation)}`);
 		await ctx.audit.log(`ipAddress: ${stringify(ipAddress)}`);
+		await ctx.audit.log(`messageId: ${stringify(messageId)}`);
 
 		if ("CONFIRMED" === confirmation) {
 			await ctx.message.respond(
@@ -56,6 +57,7 @@ export const handler: CommandHandler = async ctx => {
 					ctx,
 				),
 			);
+			reason = `ELB Access granted for ${ipAddress}/32`;
 		} else {
 			await ctx.message.respond(
 				slack.errorMessage(
@@ -64,6 +66,7 @@ export const handler: CommandHandler = async ctx => {
 					ctx,
 				),
 			);
+			reason = `ELB Access denied for ${ipAddress}/32`;
 		}
 	} else {
 		const {
@@ -79,8 +82,15 @@ export const handler: CommandHandler = async ctx => {
 			);
 			reason = "Declined for 127.0.0.1 IP Address";
 		} else {
+			const msgId = ts();
+			const msgOptions = {
+				id: msgId.toString(),
+				ts: msgId,
+				thread: parentMsg,
+			} as MessageOptions;
+
 			const response = await ctx.message.send(
-				questionMessage(ctx, ipAddress),
+				questionMessage(ctx, ipAddress, msgId),
 				{ users: [], channels: channel },
 				msgOptions,
 			);
@@ -98,6 +108,7 @@ export const handler: CommandHandler = async ctx => {
 function questionMessage(
 	ctx: Contextual<any, any>,
 	ipAddress: string,
+	msgId: number,
 ): SlackMessage {
 	return {
 		blocks: [
@@ -123,7 +134,7 @@ function questionMessage(
 					elementForCommand(
 						{
 							type: "button",
-							style: "primary",
+							// style: "primary",
 							text: {
 								type: "plain_text",
 								text: emoji("white_check_mark"),
@@ -131,7 +142,11 @@ function questionMessage(
 							},
 						} as ButtonElement,
 						"ipAddresser",
-						{ confirmation: "CONFIRMED", ipAddress: ipAddress },
+						{
+							confirmation: "CONFIRMED",
+							ipAddress: ipAddress,
+							messageId: msgId,
+						},
 					),
 					elementForCommand(
 						{
@@ -143,7 +158,11 @@ function questionMessage(
 							},
 						} as ButtonElement,
 						"ipAddresser",
-						{ confirmation: "DENIED", ipAddress: ipAddress },
+						{
+							confirmation: "DENIED",
+							ipAddress: ipAddress,
+							messageId: msgId,
+						},
 					),
 				],
 			} as ActionsBlock,
