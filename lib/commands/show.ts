2549e94f3bc21eb771fb92bf2b9ed501c3c5f950
Tim Sparg <timothy.sparg@gmail.com>
2021-05-14 13:39:21 +0200

    question message - block was null

--- lib/commands/show.ts
+++ lib/commands/show.ts
@@ -59,22 +59,24 @@ function showQuestionMessage(ctx: CommandContext): SlackMessage {
 			footer: footer(ctx),
 		},
 	);
-	msg.blocks.push({ type: "divider" } as slack.DividerBlock);
-	msg.blocks.push({
-		type: "context",
-		elements: [
-			{
-				type: "image",
-				image_url:
-					"https://image.freepik.com/free-photo/red-drawing-pin_1156-445.jpg",
-				alt_text: "images",
-			},
-			{
-				type: "mrkdwn",
-				text: "Location: *Dogpatch*",
-			},
-		],
-	} as slack.ContextBlock);
+	msg.blocks = [
+		{ type: "divider" } as slack.DividerBlock,
+		{
+			type: "context",
+			elements: [
+				{
+					type: "image",
+					image_url:
+						"https://image.freepik.com/free-photo/red-drawing-pin_1156-445.jpg",
+					alt_text: "images",
+				},
+				{
+					type: "mrkdwn",
+					text: "Location: *Dogpatch*",
+				},
+			],
+		} as slack.ContextBlock,
+	];
 
 	return msg;
 }
