1fadc684731a893df372572f8f4aaba9617e4196
Tim Sparg <timothy.sparg@gmail.com>
2021-05-16 13:05:42 +0200

    Update comment in deleteMessage

--- lib/commands/show.ts
+++ lib/commands/show.ts
@@ -311,7 +311,7 @@ async function deleteMessage(ctx: CommandContext) {
 	);
 
 	await new Promise(resolve => setTimeout(resolve, 10000));
-	await info(`deleteMessage with msgId: ${msgId}`);
+	await info(`delete sent message with msgId : ${msgId}`);
 
 	await ctx.message.delete({ channels: getChannelId(ctx) }, { id: msgId });
 }
