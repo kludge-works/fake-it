87a5d666a090f10521b0f12db2a2a94f136a50fe
Tim Sparg <timothy.sparg@gmail.com>
2021-05-16 13:47:35 +0200

    no destinations on delete

--- lib/commands/show.ts
+++ lib/commands/show.ts
@@ -321,7 +321,7 @@ async function deleteMessage(ctx: CommandContext) {
 			],
 		},
 		{
-			channels: getChannelName(ctx),
+			// channels: getChannelName(ctx),
 		},
 		{ id: msgId },
 	);
@@ -329,7 +329,12 @@ async function deleteMessage(ctx: CommandContext) {
 	await new Promise(resolve => setTimeout(resolve, 10000));
 	await info(`delete sent message with msgId : ${msgId}`);
 
-	await ctx.message.delete({ channels: getChannelName(ctx) }, { id: msgId });
+	await ctx.message.delete(
+		{
+			// channels: getChannelName(ctx) },
+		},
+		{ id: msgId },
+	);
 }
 
 async function blockMessage(ctx: CommandContext) {
